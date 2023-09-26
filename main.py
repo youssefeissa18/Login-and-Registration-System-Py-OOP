a6433cf9704273eba96deb364303469323e9a332
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2023-09-26 22:20:22 +0300

    Create Class User

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+class User:
+    def __init__(self, name, password, email, age):
+        self.name = name
+        self.password = password
+        self.email = email
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_password(self):
+        return self.password
+
+
+
+
+print("============================== Login and Registration System ==============================")
+print("1. Register User\n2. Login User\n3. Show User List\n4. Search User\n5. Delete User")
