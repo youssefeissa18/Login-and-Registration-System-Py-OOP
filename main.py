337fd7d763abb615c4f66ca57421e1befe9b2fc7
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2023-09-26 22:22:10 +0300

    Create Class Manage User And Function for register in class

--- main.py
+++ main.py
@@ -11,7 +11,19 @@ class User:
     def get_password(self):
         return self.password
 
+class UserManage:
+    def __init__(self):
+        self.Users = []
 
+    def  RegisterUser(self, name, password, email, age):
+        name = input("Enter Your Name : ")
+        password = input("Enter Your Password : ")
+        email = input("Enter Your Email : ")
+        age = int(input("Enter Your Age : "))
+        new_User = User(name, password,email,age)
+        self.Users.append(new_User)
+        print("Your Registration User Do Successfully")
+    
 
 
 print("============================== Login and Registration System ==============================")
